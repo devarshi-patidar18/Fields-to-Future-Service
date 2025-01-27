6b1b354f332df64d8a20689486bb4d04c783d5c1
devarshi-patidar1812 <devarshi330@gmail.com>
2025-01-27 23:48:37 +0530

    Project Base Setup

---
+++ Fields-To-Future-Services/src/test/java/com/apis/Fields_To_Future_Services/FieldsToFutureServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apis.Fields_To_Future_Services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldsToFutureServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
